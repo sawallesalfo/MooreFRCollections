9224da326b2ae4ebd89dc47ac2d4c4a53afb3d20
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-25 20:03:55 +0100

    🚚 run for 1-Kibayã only

--- scripts/job_segmented_audios.py
+++ scripts/job_segmented_audios.py
@@ -84,7 +84,7 @@ def main():
 
     # Configuration
     bucket_name = "moore-collection"
-    source_folder = "raw_data/yikri"
+    source_folder = "raw_data/1-Kibayã"
     destination_folder = "fasoai-segmented_audios"
     local_download_folder = "downloaded_audio"
 
